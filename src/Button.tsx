cac066ef496adf8d06fca8427e1bd0f6defe6b15
AliReza99 <alirezawbhr@gmail.com>
2024-09-20 16:06:20 +0330

    feat: add support for tailwindcss and css styles

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// tailwind classes will be remain untouched but should be imported in source application to be recognized by tailwind server
+
 export function Button({ label }: { label: string }) {
-  return <button>{label}</button>;
+  return <button className="custom-button">{label}</button>;
 }
